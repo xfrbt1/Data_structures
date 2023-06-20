21c6c9c59dc8f679d6a2f68f2fd635db60d26eea
aleksej <xfrbt1@gmail.com>
2023-06-20 17:22:27 +0300

    Initial commit

---
+++ graphs.py
@@ -0,0 +1,75 @@
+class Graph:
+    def __init__(self, nodes):
+        self.nodes = nodes
+        self.VE_DICT = dict()
+
+        for node in self.nodes:
+            self.VE_DICT[node] = list()
+
+    def new_node(self, node_name):
+        if node_name not in self.VE_DICT:
+            self.VE_DICT[node_name] = list()
+
+    def create_links(self, link_list):
+        for i in link_list:
+            if i[0] in self.VE_DICT:
+                self.VE_DICT[i[0]].append([i[1], i[2]])
+                self.VE_DICT[i[1]].append([i[0], i[2]])
+
+    def check_adj(self, node1, node2):
+        for i in self.VE_DICT[node1]:
+            if i[0] == node2:
+                return 1
+        else:
+            return 0
+
+    def adj_matrix(self):
+
+        matrix = [
+            [0 for i in range(len(self.VE_DICT))]
+            for j in range(len(self.VE_DICT))
+        ]
+
+        index_i = 0
+        for i in self.VE_DICT:
+            index_j = 0
+            for j in self.VE_DICT:
+                if self.check_adj(i, j):
+                    matrix[index_i][index_j] = 1
+                index_j += 1
+            index_i += 1
+
+        for i in matrix:
+            print(i)
+
+        return matrix
+
+    def ind_set(self):
+        adj_matrix = self.adj_matrix()
+
+        ind_list = [
+            [n for n in self.VE_DICT]
+            for i in range(len(self.VE_DICT))
+        ]
+
+        # for i in range(len(ind_list)):
+        #     for j in range(len(ind_list)):
+        #         print(f'{ind_list[i][j]} {adj_matrix[i][j]} |', end=' ')
+        #     print(f"->{i}\n")
+
+        for i in range(len(ind_list)):
+            for j in range(len(ind_list)):
+                if adj_matrix[i][j] != 0:
+                    ind_list[i][j] = 'x'
+
+        for i in ind_list:
+            print(i)
+
+    def max_ind_set(self):
+        pass
+
+    def print_all_nodes(self):
+        for k, v in self.VE_DICT.items():
+            print(f"{k} -> {v}")
+
+
---
+++ run.py
@@ -0,0 +1,34 @@
+from graphs import Graph
+
+nodes = [1, 2, 3, 4, 5, 6, 7, 8]
+link_list = [[1, 2, 1],
+             [1, 4, 1],
+             [1, 6, 1],
+
+             [2, 4, 1],
+             [2, 6, 1],
+             [2, 3, 1],
+             [2, 8, 1],
+
+             [3, 5, 1],
+             [3, 7, 1],
+             [3, 8, 1],
+
+             [4, 6, 1],
+
+             [5, 7, 1],
+             [5, 8, 1],
+
+             [6, 7, 1],
+             [6, 8, 1],
+             ]
+
+graph = Graph(nodes)
+
+graph.create_links(link_list)
+graph.print_all_nodes()
+graph.adj_matrix()
+# graph.ind_set()
+
+# graph.ind_set()
+
